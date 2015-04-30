8855c975c7d9e5c4ca4512ad67b077b5dca0d9e8
lonalore <lonalore@freemail.hu>
2015-04-30 11:02:24 +0200

    create upgrade path

--- testimonials_setup.php
+++ testimonials_setup.php
@@ -31,6 +31,7 @@ class testimonials_setup
 		// Check old database tables exist.
 		if($db->gen("SELECT * FROM #testimonials_conf WHERE 1"))
 		{
+			// Retrieve testimonial items from old table.
 			$db->gen("SELECT * FROM #testimonials WHERE 1");
 
 			$old_records = array();
@@ -79,7 +80,6 @@ class testimonials_setup
 				);
 
 				$db->insert("testimonials", $insert);
-
 				$order++;
 			}
 
@@ -108,11 +108,67 @@ class testimonials_setup
 	 */
 	function upgrade_required($var)
 	{
+		$db = e107::getDb('testimonials');
+
+		// Check old database tables exist.
+		if($db->gen("SELECT * FROM #testimonials_conf WHERE 1"))
+		{
+			return true;
+		}
 	}
 
 
 	function upgrade_pre($var)
 	{
+		$db = e107::getDb('testimonials');
+
+		// Check old database tables exist.
+		if($db->gen("SELECT * FROM #testimonials_conf WHERE 1"))
+		{
+			// Retrieve testimonial items from old table.
+			$db->gen("SELECT * FROM #testimonials WHERE 1");
+			$old_records = array();
+			while($row = $db->fetch())
+			{
+				$old_records[] = $row;
+			}
+
+			// Drop old tables.
+			$db->gen("DROP TABLE #testimonials");
+			$db->gen("DROP TABLE #testimonials_conf");
+
+			// Create new table.
+			$db->gen("CREATE TABLE #testimonials (
+				tm_id int(11) unsigned NOT NULL auto_increment,
+				tm_name varchar(50) NOT NULL DEFAULT '',
+				tm_url varchar(255) NOT NULL DEFAULT '',
+				tm_message text NOT NULL,
+				tm_datestamp int(10) unsigned NOT NULL DEFAULT '0',
+				tm_blocked tinyint(3) unsigned NOT NULL DEFAULT '0',
+				tm_ip varchar(45) NOT NULL DEFAULT '',
+				tm_order tinyint(3) unsigned NOT NULL DEFAULT '0',
+				PRIMARY KEY  (tm_id)
+				) ENGINE=MyISAM;");
+
+			// Insert testimonial items into new table.
+			$order = 0;
+			foreach($old_records as $old_record)
+			{
+				$insert = array(
+					'tm_id'        => 0,
+					'tm_name'      => '0.' . $old_record['name'],
+					'tm_url'       => $old_record['homepage'],
+					'tm_message'   => $old_record['text'],
+					'tm_datestamp' => time(),
+					'tm_blocked'   => ($old_record['allowed'] == 'yes' ? 0 : 1),
+					'tm_ip'        => '',
+					'tm_order'     => $order,
+				);
+
+				$db->insert("testimonials", $insert);
+				$order++;
+			}
+		}
 	}
 
 
