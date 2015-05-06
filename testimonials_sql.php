10697d860c2f4fd07cbe99c50f135d22bd1703b3
lonalore <lonalore@freemail.hu>
2015-05-06 15:44:08 +0200

    removed sql comment from tables

--- testimonials_sql.php
+++ testimonials_sql.php
@@ -1,11 +1,11 @@
 CREATE TABLE `testimonials` (
 `tm_id` int(11) unsigned NOT NULL auto_increment,
-`tm_name` varchar(50) NOT NULL DEFAULT '',
-`tm_url` varchar(255) NOT NULL DEFAULT '',
-`tm_message` text NOT NULL,
-`tm_datestamp` int(10) unsigned NOT NULL DEFAULT '0',
-`tm_blocked` tinyint(3) unsigned NOT NULL DEFAULT '0',
-`tm_ip` varchar(45) NOT NULL DEFAULT '',
-`tm_order` tinyint(3) unsigned NOT NULL DEFAULT '0',
+`tm_name` varchar(50) NOT NULL DEFAULT '' COMMENT 'Author anme.',
+`tm_url` varchar(255) NOT NULL DEFAULT '' COMMENT 'URL.',
+`tm_message` text NOT NULL COMMENT 'Testimonial message.',
+`tm_datestamp` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Created at..',
+`tm_blocked` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'Blocked or not.',
+`tm_ip` varchar(45) NOT NULL DEFAULT '' COMMENT 'Author IP.',
+`tm_order` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'Order.',
 PRIMARY KEY  (`tm_id`)
-) ENGINE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
