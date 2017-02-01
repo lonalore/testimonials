4e2a0e98191b6d9d587e22bc515422467c4a7efa
Norwayman <johnarild.foss@gmail.com>
2017-02-01 17:29:45 -0600

    Spanish language files
    Spanish language files

---
+++ languages/Spanish/Spanish_front.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Definition of english strings for area Frontend.
+ */
+
+define('LAN_TESTIMONIALS_01', 'Testimonios');
+define('LAN_TESTIMONIALS_02', 'Usted no tiene los permisos correctos para ver esta página.');
+define('LAN_TESTIMONIALS_03', 'Tu nombre');
+define('LAN_TESTIMONIALS_04', 'Tu mensaje');
+define('LAN_TESTIMONIALS_05', 'Enviar');
+define('LAN_TESTIMONIALS_06', 'Enviar testimonio');
+define('LAN_TESTIMONIALS_07', 'Página principal');
+define('LAN_TESTIMONIALS_08', '"Su nombre" es obligatorio Campo!');
+define('LAN_TESTIMONIALS_09', '"Su mensaje" es obligatorio Campo!');
+define("LAN_TESTIMONIALS_10", 'Incapaz de aceptar entrada no tiene ese nombre de usuario se ha registrado - si es su nombre de usuario Inicia sesión para enviar un mensaje.');
+define('LAN_TESTIMONIALS_11', 'Su mensaje ha sido guardado con éxito! ¡Gracias!');
