e5095251ee44642b777596fd016746ec3351dfef
Norwayman <johnarild.foss@gmail.com>
2017-02-01 17:28:31 -0600

    Norwegian language files
    Norwegian language files

---
+++ languages/Norwegian/Norwegian_admin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * Definition of english strings for Admin UI.
+ */
+
+define('LAN_TESTIMONIALS_ADMIN_01', 'Innstillinger');
+define('LAN_TESTIMONIALS_ADMIN_02', 'Antall elementer i menyen');
+define('LAN_TESTIMONIALS_ADMIN_03', 'Trim meldinger til en maksimal lengde');
+define('LAN_TESTIMONIALS_ADMIN_04', 'Brukerklasse som kan sende inn');
+define('LAN_TESTIMONIALS_ADMIN_05', 'Bruk captcha når skjemaet sendes inn');
+define('LAN_TESTIMONIALS_ADMIN_06', 'Godkjenning er påkrevet?');
+
+define('LAN_TESTIMONIALS_ADMIN_07', 'Uttalelser');
+define('LAN_TESTIMONIALS_ADMIN_08', 'Uttalelser');
+define('LAN_TESTIMONIALS_ADMIN_09', 'ID');
+define('LAN_TESTIMONIALS_ADMIN_10', 'Forfatter');
+define('LAN_TESTIMONIALS_ADMIN_11', 'URL');
+define('LAN_TESTIMONIALS_ADMIN_12', 'Blokkert');
+define('LAN_TESTIMONIALS_ADMIN_13', 'Nei (aktiv)');
+define('LAN_TESTIMONIALS_ADMIN_14', 'Ja (inaktiv)');
+define('LAN_TESTIMONIALS_ADMIN_15', 'Melding');
+define('LAN_TESTIMONIALS_ADMIN_16', 'Rekkefølge');
+define('LAN_TESTIMONIALS_ADMIN_17', 'Valg');
---
+++ languages/Norwegian/Norwegian_global.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Definition of english strings for common usage.
+ */
+
+define('LAN_PLUGIN_TESTIMONIALS_NAME', 'Uttalelser');
+define('LAN_PLUGIN_TESTIMONIALS_SUMM', 'Enkel Uttalelses utvidelse.');
+define('LAN_PLUGIN_TESTIMONIALS_DESC', 'Enkel Uttalelses utvidelse.');
+define('LAN_PLUGIN_TESTIMONIALS_LINK', '');
